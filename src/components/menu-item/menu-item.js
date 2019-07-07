67947bb1c880b6a1613f38ab81f8b3540dd729b8
Joey Dye <joey.m.dye@gmail.com>
2019-07-06 20:48:39 -0500

    feat: broken homepage into smaller components

--- src/components/menu-item/menu-item.js
+++ src/components/menu-item/menu-item.js
@@ -4,12 +4,13 @@ import './menu-item.sass';
 const MenuItem = ({ title, image, size }) => {
   console.log(image);
   return (
-    <div
-      style={{ backgroundImage: `url(${image})` }}
-      className={`${size} menu-item`}
-    >
+    <div className={`${size} menu-item`}>
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${image})` }}
+      />
       <div className="content">
-        <h1 className="title">{title}</h1>
+        <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">SHOP NOW</span>
       </div>
     </div>
